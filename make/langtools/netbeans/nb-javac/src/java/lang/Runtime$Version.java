029a3134462a4dcf594a84b81421cc27419facb5
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2022-01-11 08:55:11 +0100

    Fake just Runtime.Version class

---
+++ make/langtools/netbeans/nb-javac/src/java/lang/Runtime$Version.java
@@ -0,0 +1,14 @@
+package java.lang;
+
+public final class Runtime$Version {
+    private Runtime$Version() {
+    }
+
+    public int feature() {
+        return 8;
+    }
+
+    public static Runtime$Version parse(String v) {
+        throw new IllegalStateException();
+    }
+}
