1c6acf4c26172973075d4027ca39a36c063ad43b
Jaroslav Tulach <jaroslav.tulach@oracle.com>
2022-03-02 16:32:19 +0100

    Dynamically detect the Java version when building patched JDK Embed delegating PlatformDescription at the call site.

--- build-jdk.sh
+++ build-jdk.sh
@@ -1,14 +1,37 @@
-if ! [ -f $JAVA_HOME17/bin/javac ]; then
-  echo Specify JAVA_HOME17 environment variable!
+#/bin/bash
+set -e
+
+if [ "x$JAVA_HOME_VERSION" == "x" ]; then
+  if ! [ -f $JAVA_HOME/bin/javac ]; then
+    if ! [ -f $JAVA_HOME17/bin/javac ]; then
+      echo Specify JAVA_HOME environment variable!
+      exit 1
+    fi
+    JAVA_HOME_VERSION=$JAVA_HOME17
+  else
+    JAVA_HOME_VERSION=$JAVA_HOME
+  fi
+fi
+
+echo Using Java at $JAVA_HOME_VERSION
+
+if [ "x$JAVA_VERSION" == "x" ]; then
+  $JAVA_HOME_VERSION/bin/java -version
+  JAVA_VERSION=`$JAVA_HOME_VERSION/bin/java -version 2>&1 | grep version | head -n 1 | cut -f 2 -d \" | cut -f 1 -d - | cut -f 1 -d .`
+  echo Detected Java version: $JAVA_VERSION
+fi
+
+if ! [ -f $JAVA_HOME_VERSION/bin/javac ]; then
+  echo Specify JAVA_HOME_VERSION environment variable!
   exit 1
 fi
 
 rm -rf build/test
 mkdir -p build/test
 mkdir -p build/test/modules
-cp $JAVA_HOME17/jmods/* build/test/modules
+cp $JAVA_HOME_VERSION/jmods/* build/test/modules
 #clear ModuleHashes on java.base:
-$JAVA_HOME17/bin/java --add-modules jdk.jdeps --add-exports jdk.jdeps/com.sun.tools.classfile=ALL-UNNAMED StripModuleHashes.java $JAVA_HOME17/jmods/java.base.jmod build/test/modules/java.base.jmod
+$JAVA_HOME_VERSION/bin/java --add-modules jdk.jdeps --add-exports jdk.jdeps/com.sun.tools.classfile=ALL-UNNAMED StripModuleHashes.java $JAVA_HOME_VERSION/jmods/java.base.jmod build/test/modules/java.base.jmod
 
 #prepare:
 
@@ -45,7 +68,7 @@ cp -r make/langtools/netbeans/nb-javac/build/classes/jdk build/test/out/jdk.comp
 rm -rf build/test/expanded/jdk.compiler
 mkdir -p build/test/expanded/jdk.compiler
 
-$JAVA_HOME17/bin/jmod extract --dir=build/test/expanded/jdk.compiler $JAVA_HOME17/jmods/jdk.compiler.jmod
+$JAVA_HOME_VERSION/bin/jmod extract --dir=build/test/expanded/jdk.compiler $JAVA_HOME_VERSION/jmods/jdk.compiler.jmod
 
 #jdk.jdeps:
 rm build/test/modules/jdk.jdeps.jmod
@@ -58,7 +81,7 @@ mkdir -p build/test/out/jdk.jdeps
 rm -rf build/test/expanded/jdk.jdeps
 mkdir -p build/test/expanded/jdk.jdeps
 
-$JAVA_HOME17/bin/jmod extract --dir=build/test/expanded/jdk.jdeps $JAVA_HOME17/jmods/jdk.jdeps.jmod
+$JAVA_HOME_VERSION/bin/jmod extract --dir=build/test/expanded/jdk.jdeps $JAVA_HOME_VERSION/jmods/jdk.jdeps.jmod
 
 cp -r build/test/expanded/jdk.jdeps/classes/* build/test/out/jdk.jdeps/
 
@@ -70,12 +93,23 @@ mkdir -p build/test/out/jdk.jdeps/com/sun/tools/javap
 cp -r make/langtools/netbeans/nb-javac/build/classes/com/sun/tools/javap build/test/out/jdk.jdeps/com/sun/tools/
 
 #build module-infos:
-$JAVA_HOME17/bin/javac --module-source-path build/test/src/ -d build/test/out `find build/test/src/ -type f -name "*.java"`
+$JAVA_HOME_VERSION/bin/javac --module-source-path build/test/src/ -d build/test/out `find build/test/src/ -type f -name "*.java"`
 
 #build jmods:
-$JAVA_HOME17/bin/jmod create --class-path build/test/out/java.compiler/ --module-version 17 build/test/modules/java.compiler.jmod
-$JAVA_HOME17/bin/jmod create --class-path build/test/out/jdk.compiler/ --cmds build/test/expanded/jdk.compiler/bin/ --legal-notice build/test/expanded/jdk.compiler/legal/ --libs build/test/expanded/jdk.compiler/lib/ --man-pages build/test/expanded/jdk.compiler/man/ --module-version 17 build/test/modules/jdk.compiler.jmod
-$JAVA_HOME17/bin/jmod create --class-path build/test/out/jdk.jdeps/ --cmds build/test/expanded/jdk.jdeps/bin/ --legal-notice build/test/expanded/jdk.jdeps/legal/ --man-pages build/test/expanded/jdk.jdeps/man/ --module-version 17 build/test/modules/jdk.jdeps.jmod
+$JAVA_HOME_VERSION/bin/jmod create --class-path build/test/out/java.compiler/ --module-version $JAVA_VERSION build/test/modules/java.compiler.jmod
+$JAVA_HOME_VERSION/bin/jmod create --class-path build/test/out/jdk.compiler/ --cmds build/test/expanded/jdk.compiler/bin/ --legal-notice build/test/expanded/jdk.compiler/legal/ --libs build/test/expanded/jdk.compiler/lib/ --man-pages build/test/expanded/jdk.compiler/man/ --module-version $JAVA_VERSION build/test/modules/jdk.compiler.jmod
+$JAVA_HOME_VERSION/bin/jmod create --class-path build/test/out/jdk.jdeps/ --cmds build/test/expanded/jdk.jdeps/bin/ --legal-notice build/test/expanded/jdk.jdeps/legal/ --man-pages build/test/expanded/jdk.jdeps/man/ --module-version $JAVA_VERSION build/test/modules/jdk.jdeps.jmod
 
 #build image:
-$JAVA_HOME17/bin/jlink -p build/test/modules:$JAVA_HOME17/jmods/ --add-modules ALL-MODULE-PATH --output build/test/jdk
+$JAVA_HOME_VERSION/bin/jlink -p build/test/modules:$JAVA_HOME_VERSION/jmods/ --add-modules ALL-MODULE-PATH --output build/test/jdk
+
+# test
+
+echo "class Demo {" > build/test/Demo.java
+echo "  public static void main(String[] args) {" >> build/test/Demo.java
+echo '    System.out.println("Find patched Java version " + System.getProperty("java.version") + " at " + System.getProperty("java.home"));' >> build/test/Demo.java
+echo "  }" >> build/test/Demo.java
+echo "}" >> build/test/Demo.java
+
+build/test/jdk/bin/javac --release 11 -d build/test/ build/test/Demo.java
+build/test/jdk/bin/java -cp build/test/ Demo
